6158d2811ca4e73e14dbf7aafc6c1bb0d2e20b1a
hqin-2020 <uctphqi@ucl.ac.uk>
2022-07-09 23:42:25 -0500

    2

--- src_final/Final_distbution.py
+++ src_final/Final_distbution.py
@@ -42,12 +42,10 @@ t = 282
 θ_final = []
 success_seed = []
 for i in tqdm(range(1,batch_num)):
-    try: 
-        with open(casedir + 'θ_'+str(t)+'.pkl', 'rb') as f:
-            θ_final.append(pickle.load(f))
-        success_seed.append(i)
-    except:
-        print(i)
+
+    with open(casedir + 'θ_'+str(t)+'.pkl', 'rb') as f:
+        θ_final.append(pickle.load(f))
+    success_seed.append(i)
 
 def return_coll(θ_final):
     λ_particle = []; η_particle = []; b11_particle = []; b22_particle = []
