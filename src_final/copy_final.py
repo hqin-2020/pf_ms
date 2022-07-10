7d6ee750b63ffdf1d47ac485f465c2e7f1929b9e
hqin-2020 <uctphqi@ucl.ac.uk>
2022-07-10 10:33:03 -0500

    4

--- src_final/copy_final.py
+++ src_final/copy_final.py
@@ -14,6 +14,7 @@ for i in tqdm(range(1,batch_num)):
     case = 'actual data, seed = ' + str(i) + ', T = ' + str(T) + ', N = ' + str(N)
     casedir = destination_dir + case  + '/'
     try:
+        os.mkdir(casedir)
         shutil.copy(source_dir + case  + '/θ_282.pkl', casedir)
     except:
         pass
