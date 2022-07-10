49589642c2b5875714e0826f00095322dc0793bc
hqin-2020 <uctphqi@ucl.ac.uk>
2022-07-10 10:29:39 -0500

    4

--- src_final/copy_final.py
+++ src_final/copy_final.py
@@ -7,10 +7,13 @@ source_dir = '/scratch/qhaomin/pf_ms/'
 destination_dir = workdir + '/output/'
 
 N = 100_000
-T = 282
+T = 283
 batch_num = 135
 
 for i in tqdm(range(1,batch_num)):
     case = 'actual data, seed = ' + str(i) + ', T = ' + str(T) + ', N = ' + str(N)
     casedir = destination_dir + case  + '/'
-    shutil.copy(source_dir + case  + '/θ_282.pkl', casedir)
+    try:
+        shutil.copy(source_dir + case  + '/θ_282.pkl', casedir)
+    except:
+        pass
